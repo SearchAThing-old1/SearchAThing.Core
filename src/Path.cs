e5ab6ae3b127f8dd3675d640aa146b25c13e0413
Lorenzo Delana <oss.devel@searchathing.com>
2016-06-06 23:41:43 +0200

    - process task helper to manage pipeline ( stdin, stdout, stderr ) through Task - search in path ext method to give full pathname of an executable

---
+++ src/Path.cs
@@ -0,0 +1,54 @@
+﻿#region SearchAThing.Core, Copyright(C) 2015-2016 Lorenzo Delana, License under MIT
+/*
+* The MIT License(MIT)
+* Copyright(c) 2016 Lorenzo Delana, https://searchathing.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+#endregion
+
+using System;
+using System.IO;
+
+namespace SearchAThing
+{
+
+    public static partial class Extensions
+    {
+        
+        /// <summary>
+        /// Search given filename in the PATH
+        /// </summary>
+        /// <returns>null if not found</returns>
+        public static string SearchInPath(this string filename)
+        {
+            if (File.Exists(filename)) return Path.GetFullPath(filename);
+
+            var paths = Environment.GetEnvironmentVariable("PATH");
+            foreach (var path in paths.Split(';'))
+            {
+                var pathname = Path.Combine(path, filename);
+                if (File.Exists(pathname)) return pathname;
+            }
+            return null;
+        }
+
+    }
+
+}
