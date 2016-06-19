349366c52ed69493fd00ff853807fc6ef1715d3c
Lorenzo Delana <oss.devel@searchathing.com>
2016-06-19 19:33:32 +0200

    AppDataFolder, EnsureFolder, SearchInPath helpers

--- src/Path.cs
+++ src/Path.cs
@@ -25,29 +25,62 @@
 
 using System;
 using System.IO;
+using System.Reflection;
 
 namespace SearchAThing
 {
 
+    namespace Core
+    {
+
+        public static class Path
+        {
+
+            /// <summary>
+            /// {AppData}/{namespace}/{assembly_name}
+            /// </summary>
+            public static string AppDataFolder(string ns)
+            {
+                return EnsureFolder(System.IO.Path.Combine(
+                    System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ns),
+                    Assembly.GetCallingAssembly().GetName().Name));
+
+            }
+
+            /// <summary>
+            /// Ensure given folder path exists.
+            /// </summary>        
+            public static string EnsureFolder(this string path)
+            {
+                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                return path;
+            }
+
+            /// <summary>
+            /// Search given filename in the PATH
+            /// </summary>
+            /// <returns>null if not found</returns>
+            public static string SearchInPath(this string filename)
+            {
+                if (File.Exists(filename)) return System.IO.Path.GetFullPath(filename);
+
+                var paths = Environment.GetEnvironmentVariable("PATH");
+                foreach (var path in paths.Split(';'))
+                {
+                    var pathname = System.IO.Path.Combine(path, filename);
+                    if (File.Exists(pathname)) return pathname;
+                }
+                return null;
+            }
+
+        }
+
+    }
+
     public static partial class Extensions
     {
-        
-        /// <summary>
-        /// Search given filename in the PATH
-        /// </summary>
-        /// <returns>null if not found</returns>
-        public static string SearchInPath(this string filename)
-        {
-            if (File.Exists(filename)) return Path.GetFullPath(filename);
 
-            var paths = Environment.GetEnvironmentVariable("PATH");
-            foreach (var path in paths.Split(';'))
-            {
-                var pathname = Path.Combine(path, filename);
-                if (File.Exists(pathname)) return pathname;
-            }
-            return null;
-        }
+
 
     }
 
