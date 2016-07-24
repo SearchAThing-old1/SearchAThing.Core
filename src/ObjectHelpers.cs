f5e5aa715159b51ace41f2194f99356e43b4f72d
Lorenzo Delana <oss.devel@searchathing.com>
2016-07-24 12:36:14 +0200

    TaggedObject extensions

--- src/ObjectHelpers.cs
+++ src/ObjectHelpers.cs
@@ -23,17 +23,20 @@
 */
 #endregion
 
+using SearchAThing.Core;
 using System;
 using static System.Math;
 
 namespace SearchAThing
-{   
+{
 
     namespace Core
     {
 
         public class TaggedObject<O, T>
         {
+
+            public TaggedObject(O o, T t) { Obj = o; Tag = t; }
 
             public O Obj { get; set; }
 
@@ -44,6 +47,8 @@ namespace SearchAThing
         public class TaggedObject<O, T1, T2>
         {
 
+            public TaggedObject(O o, T1 t1, T2 t2) { Obj = o; Tag1 = t1; Tag2 = t2; }
+
             public O Obj { get; set; }
 
             public T1 Tag1 { get; set; }
@@ -53,6 +58,7 @@ namespace SearchAThing
 
         public class TaggedObject<O, T1, T2, T3>
         {
+            public TaggedObject(O o, T1 t1, T2 t2, T3 t3) { Obj = o; Tag1 = t1; Tag2 = t2; Tag3 = t3; }
 
             public O Obj { get; set; }
 
@@ -61,6 +67,15 @@ namespace SearchAThing
             public T3 Tag3 { get; set; }
 
         }
+
+    }
+
+    public static partial class Extensions
+    {
+
+        public static TaggedObject<O, T> TaggedObject<O, T>(this O o, T t) { return new TaggedObject<O, T>(o, t); }
+        public static TaggedObject<O, T1, T2> TaggedObject<O, T1, T2>(this O o, T1 t1, T2 t2) { return new TaggedObject<O, T1, T2>(o, t1, t2); }
+        public static TaggedObject<O, T1, T2, T3> TaggedObject<O, T1, T2, T3>(this O o, T1 t1, T2 t2, T3 t3) { return new TaggedObject<O, T1, T2, T3>(o, t1, t2, t3); }
 
     }
 
