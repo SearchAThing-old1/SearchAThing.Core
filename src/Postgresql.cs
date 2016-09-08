c626a04aabcb83f19443cf5f24fd199b6404b49f
Lorenzo Delana <oss.devel@searchathing.com>
2016-09-08 22:40:38 +0200

    postgresql helpers

--- src/Postgresql.cs
+++ src/Postgresql.cs
@@ -23,14 +23,34 @@
 */
 #endregion
 
+using Npgsql;
 using SearchAThing.Core;
 using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.Globalization;
 using System.Text;
 using static System.Math;
+using System.Collections;
 
 namespace SearchAThing
 {
+
+    public static partial class Dynamic
+    {
+
+        /// <summary>
+        /// return the object cast to type T
+        /// or specified defaultValue if the obj is a DBNull.Value
+        /// </summary>
+        public static T FromPsql<T>(dynamic obj, T defaultValue)
+        {
+            if (obj is DBNull && obj == DBNull.Value) return defaultValue;
+
+            return (T)obj;
+        }
+
+    }
 
     public static partial class Extensions
     {
@@ -41,9 +61,10 @@ namespace SearchAThing
         /// </summary>
         public static T FromPsql<T>(this object obj, T defaultValue)
         {
-            if (obj == DBNull.Value) return defaultValue;
+            return Dynamic.FromPsql<T>(obj, defaultValue);
+            /*if (obj == DBNull.Value) return defaultValue;
 
-            return (T)obj;
+            return (T)obj;*/
         }
 
         /// <summary>
@@ -52,6 +73,7 @@ namespace SearchAThing
         /// </summary>        
         public static string ToPsql(this bool value)
         {
+
             if (value)
                 return "TRUE";
             else
@@ -188,8 +210,192 @@ namespace SearchAThing
 
                 return sb.ToString();
             }
+
         }
 
+        /// <summary>
+        /// check if psql field is a null
+        /// </summary>        
+        public static bool IsPsqlNull(this object obj)
+        {
+            return obj == DBNull.Value;
+        }
+
+        /// <summary>        
+        /// executes a select of given fields from table with whereClauses
+        /// </summary>        
+        public static NpgsqlReaderEnumerable SELECT(this NpgsqlCommand cmd,
+            string[] fields,
+            string fromTable,
+            string whereClause = null)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"select {string.Join(",", fields)} from {fromTable}");
+            if (whereClause != null) sb.Append($" where {whereClause}");
+
+            cmd.CommandText = sb.ToString();
+
+            return new NpgsqlReaderEnumerable(cmd.ExecuteReader());
+        }
+
+        /// <summary>
+        /// executes a select idField from tablename where whereClause limit 1
+        /// it return 0 if not exists, >0 elsewhere
+        /// </summary>        
+        public static int FIND_RECORD_ID(this NpgsqlCommand cmd,
+            string idField,
+            string tablename,
+            string whereClause)
+        {
+            cmd.CommandText = $"select {idField} from {tablename} where {whereClause} limit 1";
+            return ((int?)cmd.ExecuteScalar()).GetValueOrDefault();
+        }
+
+        /// <summary>
+        /// checks whatever given table is empty, under optional whereClause,
+        /// by querying records count by given id field
+        /// </summary>        
+        public static bool IS_EMPTY(this NpgsqlCommand cmd,
+            string tableName,
+            string idField,
+            string whereClause = null)
+        {
+            var sb = new StringBuilder();
+            sb.Append($"select count({idField}) from {tableName}");
+            if (whereClause != null) sb.Append($" where {whereClause}");
+            cmd.CommandText = sb.ToString();
+            return ((long)cmd.ExecuteScalar()) == 0L;
+        }
+
+        /// <summary>
+        /// creates an insert into table (fields) values (val_objects) [returning idfield]
+        /// it executes a Scalar query and returns int>0 if returning_id is given, 0 otherwise executin a NonQuery
+        /// </summary>       
+        public static int INSERT(this NpgsqlCommand cmd,
+            string intoTable,
+            string[] columnNames,
+            object[] columnValues,
+            string returning_id = "id")
+        {
+            var sb = new StringBuilder();
+
+            sb.Append($"insert into {intoTable} ( ");
+            for (int i = 0; i < columnNames.Length; ++i)
+            {
+                if (i > 0) sb.Append(", ");
+                sb.Append(columnNames[i]);
+            }
+            sb.Append(" ) values ( ");
+            for (int i = 0; i < columnValues.Length; ++i)
+            {
+                if (i > 0) sb.Append(", ");
+                sb.Append(columnValues[i]);
+            }
+            sb.Append(" )");
+
+            if (!string.IsNullOrEmpty(returning_id))
+            {
+                sb.Append($" returning {returning_id}");
+
+                cmd.CommandText = sb.ToString();
+
+                return (int)cmd.ExecuteScalar();
+            }
+            else
+            {
+                cmd.CommandText = sb.ToString();
+
+                cmd.ExecuteNonQuery();
+
+                return 0;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// enumerable for NpgsqlDataReader into dynamic object
+    /// it calls automatically reader Close on dispose ( ie. enum break )
+    /// </summary>
+    public class NpgsqlReaderEnumerable : IEnumerable<object>
+    {
+        NpgsqlReaderEnumerator en = null;
+
+        public NpgsqlReaderEnumerable(NpgsqlDataReader _rdr)
+        {
+            en = new NpgsqlReaderEnumerator(_rdr);
+        }
+
+        public IEnumerator<object> GetEnumerator()
+        {
+            return en;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return en;
+        }
+    }
+
+    /// <summary>
+    /// enumerator for NpgsqlReaderEnumerable
+    /// </summary>
+    public class NpgsqlReaderEnumerator : IEnumerator<object>
+    {
+        NpgsqlDataReader rdr = null;
+
+        public NpgsqlReaderEnumerator(NpgsqlDataReader _rdr)
+        {
+            rdr = _rdr;
+        }
+
+        int off = -1;
+        object cur = null;
+
+        public object Current
+        {
+            get
+            {
+                if (off == -1) throw new Exception($"bad enum index");
+
+                if (cur == null)
+                {
+                    IDictionary<string, object> obj = new ExpandoObject();
+
+                    for (int i = 0; i < rdr.FieldCount; ++i)
+                    {
+                        var fieldName = rdr.GetName(i);
+                        obj.Add(fieldName, rdr[fieldName]);
+                    }
+
+                    cur = obj;
+                }
+
+                return cur;
+            }
+        }
+
+        public void Dispose()
+        {
+            rdr.Close();
+        }
+
+        public bool MoveNext()
+        {
+            ++off;
+
+            cur = null;
+
+            return rdr.Read();
+        }
+
+        public void Reset()
+        {
+            off = -1;
+
+            throw new Exception("not implemented");
+        }
     }
 
 }
