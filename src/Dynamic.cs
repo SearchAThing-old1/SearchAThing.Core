7af3ae8c64b70e866ea06d9d4813b3cdc17d6017
Lorenzo Delana <oss.devel@searchathing.com>
2017-06-04 09:49:53 +0200

    fix dbnull value check

--- src/Dynamic.cs
+++ src/Dynamic.cs
@@ -57,7 +57,7 @@ namespace SearchAThing
         /// </summary>        
         public static string GetString(dynamic value, string valueIfNull = "")
         {
-            if (value == null || value == DBNull.Value) return valueIfNull;
+            if (value == null || value is DBNull) return valueIfNull;
 
             return (string)value;
         }
@@ -67,7 +67,7 @@ namespace SearchAThing
         /// </summary>        
         public static bool GetBool(dynamic value, bool valueIfNull = false)
         {
-            if (value == null || value == DBNull.Value) return valueIfNull;
+            if (value == null || value is DBNull) return valueIfNull;
 
             return (bool)value;
         }
@@ -77,7 +77,7 @@ namespace SearchAThing
         /// </summary>        
         public static double GetDouble(dynamic value, double valueIfNull = 0)
         {
-            if (value == null || value == DBNull.Value) return valueIfNull;
+            if (value == null || value is DBNull) return valueIfNull;
 
             return (double)value;
         }
@@ -87,7 +87,7 @@ namespace SearchAThing
         /// </summary>        
         public static double? GetNullableDouble(dynamic value)
         {
-            if (value == null || value == DBNull.Value) return new double?();
+            if (value == null || value is DBNull) return new double?();
 
             return (double)value;
         }
@@ -97,7 +97,7 @@ namespace SearchAThing
         /// </summary>        
         public static long GetLong(dynamic value, long valueIfNull = 0L)
         {
-            if (value == null || value == DBNull.Value) return valueIfNull;
+            if (value == null || value is DBNull) return valueIfNull;
 
             return (long)value;
         }
@@ -107,7 +107,7 @@ namespace SearchAThing
         /// </summary>        
         public static int GetInt(dynamic value, int valueIfNull = 0)
         {
-            if (value == null || value == DBNull.Value) return valueIfNull;
+            if (value == null || value is DBNull) return valueIfNull;
 
             return (int)value;
         }
